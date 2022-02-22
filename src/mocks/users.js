63d2685f369b89e8930b9004124390255aba3e31
Gabriel Moura da Silva <gabrielmsdf3@gmail.com>
2022-02-21 22:05:10 -0300

    complete project

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Gabriel",
+  },
+  {
+    id: 2,
+    name: "John",
+  },
+  {
+    id: 3,
+    name: "matheus",
+  },
+];
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+const userController = require("./controllers/useController");
+module.exports = [
+  {
+    endpoint: "/users",
+    method: "GET",
+    handler: userController.listUsers,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "GET",
+    handler: userController.getUserById,
+  },
+  {
+    endpoint: "/users",
+    method: "POST",
+    handler: userController.createUser,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "PUT",
+    handler: userController.updateUser,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "DELETE",
+    handler: userController.deleteUser,
+  },
+];
